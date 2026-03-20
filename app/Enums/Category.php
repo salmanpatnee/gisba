5a37af94439f807fbfb49d399429360a43fb8bbd
salmanpatnee <salmanpatni92@gmail.com>
2026-03-20 15:33:39 +0500

    Add categories

--- app/Enums/Category.php
+++ app/Enums/Category.php
@@ -4,12 +4,23 @@ namespace App\Enums;
 
 enum Category: string
 {
-    case Compliance = 'Compliance';
-    case Cybersecurity = 'Cybersecurity';
-    case Governance = 'Governance';
+    case CybersecurityGovernance = 'Cybersecurity Governance';
+    case PoliciesAndProcedureManagement = 'Policies and Procedure Management';
     case RiskManagement = 'Risk Management';
-    case Training = 'Training';
-    case News = 'News';
+    case HumanResource = 'Human Resource';
+    case CybersecurityTrainingAndAwareness = 'Cybersecurity Training & Awareness';
+    case AssetManagement = 'Asset Management';
+    case IdentityAndAccessManagement = 'Identity and Access Management';
+    case NetworkSecurity = 'Network Security';
+    case Cryptography = 'Cryptography';
+    case BackupAndRecoveryManagement = 'Backup & Recovery Management';
+    case VulnerabilityAndPenetrationTesting = 'Vulnerability & Penetration Testing';
+    case IncidentHandling = 'Incident Handling';
+    case PhysicalSecurity = 'Physical Security';
+    case ApplicationDevelopment = 'Application Development';
+    case BusinessContinuityAndCrisesManagement = 'Business Continuity and Crises Management';
+    case SupplyChain = 'Supply Chain';
+    case Other = 'Others';
 
     /** @return array<string, string> */
     public static function options(): array
